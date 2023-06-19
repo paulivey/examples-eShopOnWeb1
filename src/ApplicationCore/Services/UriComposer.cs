9b5938246629546fbd99b52b3e975b0f0226455d
Paul Ivey <paul@iveylabs.co.uk>
2023-06-19 21:23:55 +0100

    Initial working config

---
+++ src/ApplicationCore/Services/UriComposer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using System;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services;
+
+public class UriComposer : IUriComposer
+{
+    private readonly CatalogSettings _catalogSettings;
+
+    public UriComposer(CatalogSettings catalogSettings) => _catalogSettings = catalogSettings;
+
+    public string ComposePicUri(string uriTemplate)
+    {
+        // PI - Modified to use environment variable
+        return uriTemplate.Replace("http://catalogbaseurltobereplaced", Environment.GetEnvironmentVariable("CATALOG_BASE_URL"));
+    }
+}
